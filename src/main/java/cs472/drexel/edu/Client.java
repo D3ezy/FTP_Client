e924e67b83759e3898f799c2d951d26e6f801c8a
D3ezy <matthewdey9@gmail.com>
2019-04-15 10:39:21 -0400

    initial commit

---
+++ src/main/java/cs472/drexel/edu/Client.java
@@ -0,0 +1,22 @@
+/*
+ * Client.java
+ * 
+ * Author: Matthew Dey
+ * Date Created: April 15th, 2019
+ * Drexel University
+ * CS 472 - HW2 - Computer Networks
+ */
+
+package cs472.drexel.edu;
+
+import java.net.Socket;
+
+public class Client {
+
+	private Socket s;
+	
+	Client() {
+		
+	}
+	
+}
---
+++ src/main/java/cs472/drexel/edu/Main.java
@@ -0,0 +1,20 @@
+/*
+ * Main.java
+ * 
+ * Author: Matthew Dey
+ * Date Created: April 15th, 2019
+ * Drexel University
+ * CS 472 - HW2 - Computer Networks
+ */
+
+package cs472.drexel.edu;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("This program runs.");
+		// TODO Auto-generated method stub
+
+	}
+
+}
