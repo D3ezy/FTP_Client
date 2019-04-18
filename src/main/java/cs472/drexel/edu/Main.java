ad89da852758e9b2108c38b1df4aed53603a7aeb
Matthew Dey <matthewdey9@gmail.com>
2019-04-18 15:49:37 -0400

    add menu skeleton

--- src/main/java/cs472/drexel/edu/Main.java
+++ src/main/java/cs472/drexel/edu/Main.java
@@ -9,10 +9,51 @@
 
 package cs472.drexel.edu;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("This program runs.");
+		showMenu();
+	}
+
+	public static void showMenu() {
+
+		Scanner input = new Scanner(System.in);
+		boolean isRunning = true;
+
+		while (isRunning) {
+			printMenu();
+			switch(input.nextLine()) {
+				case "USER":
+				case "PASS":
+				case "CWD":
+				case "PASV":
+				case "EPSV":
+				case "PORT":
+				case "EPRT":
+				case "RETR":
+				case "STOR":
+				case "PWD":
+				case "SYST":
+				case "LIST":
+				case "HELP":
+				case "QUIT":
+					isRunning = false;
+					break;
+				default:
+			}
+		}
+
+		return;
+	}
+
+	public static void printMenu() {
+		ArrayList<String> cmds = new ArrayList<>(Arrays.asList("USER","PASS","CWD","QUIT","PASV","EPSV","PORT",
+																"EPRT", "RETR","STOR","PWD", "SYST", "LIST", "HELP"));
+		return;
 	}
 
 }
