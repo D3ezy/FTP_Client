b60fa8734995dd1737f139343fecf23c8f63526e
Matthew Dey <matthewdey9@gmail.com>
2019-04-18 21:45:05 -0400

    add menu print to cmd

--- src/main/java/cs472/drexel/edu/Main.java
+++ src/main/java/cs472/drexel/edu/Main.java
@@ -16,17 +16,18 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		showMenu();
+		Client c = new Client();
+		showMenu(c);
 	}
 
-	public static void showMenu() {
+	public static void showMenu(Client c) {
 
 		Scanner input = new Scanner(System.in);
 		boolean isRunning = true;
 
 		while (isRunning) {
 			printMenu();
-			switch(input.nextLine()) {
+			switch(input.nextLine().toUpperCase()) {
 				case "USER":
 				case "PASS":
 				case "CWD":
@@ -40,10 +41,13 @@ public class Main {
 				case "SYST":
 				case "LIST":
 				case "HELP":
+					// Displays HELP menu
 				case "QUIT":
 					isRunning = false;
 					break;
 				default:
+					System.out.println("CMD_ERROR: Input not recognized, please try again. For usage type HELP");
+					System.out.println();
 			}
 		}
 
@@ -51,8 +55,18 @@ public class Main {
 	}
 
 	public static void printMenu() {
-		ArrayList<String> cmds = new ArrayList<>(Arrays.asList("USER","PASS","CWD","QUIT","PASV","EPSV","PORT",
-																"EPRT", "RETR","STOR","PWD", "SYST", "LIST", "HELP"));
+		ArrayList<String> cmd_col1 = new ArrayList<>(Arrays.asList("USER","PASS","CWD"));
+		ArrayList<String> cmd_col2 = new ArrayList<>(Arrays.asList("LIST","PASV","EPSV"));
+		ArrayList<String> cmd_col3 = new ArrayList<>(Arrays.asList("PORT","EPRT", "RETR"));
+		ArrayList<String> cmd_col4 = new ArrayList<>(Arrays.asList("STOR","PWD", "SYST"));
+		ArrayList<String> cmd_col5 = new ArrayList<>(Arrays.asList("QUIT", "HELP",""));
+
+		for (int i = 0; i < cmd_col1.size(); i++) {
+			System.out.printf("%-5s%-5s%-5s%-5s%-5s", cmd_col1.get(i), cmd_col2.get(i), cmd_col3.get(i), cmd_col4.get(i), cmd_col5.get(i));
+			System.out.println();
+		}
+
+		System.out.println();
 		return;
 	}
 
